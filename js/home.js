0dda9b844b0bb91bd6e5da66858f66da8993064e
smooth-runner <69725374+smooth-runner@users.noreply.github.com>
2025-05-22 20:45:39 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -2,15 +2,13 @@ var audio = document.getElementById("music");
 var allowContinue = true;
 var song = '../media/ezwinz.mp3';
 
-function proceed() 
-  document.addEventListener("click", function () {
-  document.body.classList.add("background-on");
-});
-{
+function proceed() {
   if (allowContinue) {
     document.getElementById("main").style.opacity = 1;
     document.querySelector(".enter").style.opacity = 0;
-
+      document.addEventListener("click", function () {
+  document.body.classList.add("background-on");
+});
     setTimeout(function () {
 
       audio.src = song;
